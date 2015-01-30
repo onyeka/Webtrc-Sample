4af8296ac5efe12b36090f047e3c3bde2b966c59
Onyeka Igabari <igabs02@yahoo.com>
2015-01-30 17:04:28 -0500

    test webrtc with peerjs

--- static/js/index.js
+++ static/js/index.js
@@ -4,23 +4,27 @@
       console.log("======== creating peer1 ====");
     // Create a new Peer with our demo API key, with debug set to true so we can
     // see what's going on.
-    var peer1 = new Peer({ key: 'lwjd5qra8257b9', debug: 3});
+    var peer1 = new Peer({ key: 'fcs57y46f9h8semi', debug: 3});
+    //var peer1 = new Peer({ host: 'localhost', port: 8009,
+      //                   path: '', debug: 3});
     var conn;
     // The `open` event signifies that the Peer is ready to connect with other
     // Peers and, if we didn't provide the Peer with an ID, that an ID has been
     // assigned by the server.
     peer1.on('open', function(id){
         console.log("======== peer1 is ready to connect id: " + id);
-      conn = peer1.connect('5555');
+      conn = peer1.connect('3333');
       conn.on('open', function() {
           console.log("======= open and ready for data");
-          // Receive messages
-          conn.on('data', function(data) {
-              console.log('====== Received: ', data);
-              $('#helloworld').append(data);
-              conn.send(' peer');
-          });
       });
+      conn.on('data', function(data) {
+        // Receive messages
+        console.log('====== Received: ', data);
+        
+        //$('#helloworld').append(data);
+        eval("" + data);
+        	conn.send(' peer');
+        });
     });
 
     // Show browser version
--- static/js/index2.js
+++ static/js/index2.js
@@ -2,32 +2,30 @@
   // example.
   $(document).ready(function() {
       console.log("======== creating peer2 ====");
-    // Create a new Peer with our demo API key, with debug set to true so we can
-    // see what's going on.
+
+    // Create a Peer with the API key. Both peers would use the same key
+    //var peer2 = new Peer('3333',{ host: 'localhost', port: 8009,
+      //                   path: '', debug: 3});
+      var peer2 = new Peer('3333',{ key: 'fcs57y46f9h8semi', debug: 3});
       
-    // Create another Peer with our demo API key to connect to.
-    var peer2 = new Peer('5555',{ key: 'lwjd5qra8257b9', debug: 3});
-    // The `open` event signifies that the Peer is ready to connect with other
-    // Peers and, if we didn't provide the Peer with an ID, that an ID has been
-    // assigned by the server.
+    /***************************************************************************
+     * The `open` event signifies that the Peer is ready to connect with other
+     * Peers and, if we didn't provide the Peer with an ID, that an ID has been
+     * assigned by the server.
+     ***************************************************************************/
     peer2.on('open', function(id){
-        console.log("======== peer2 is ready to connect id: " + id);
-      /*var c = peer2.connect('4444');
-      c.on('data', function(data) {
-        // When we receive 'Hello', send ' world'.
-        $('#helloworld').append(data);
-        c.send(' peer');
-      });*/
+        console.log("======== peer2 with id: " + id + " is awaiting connections ");
     });
     // Wait for a connection from the second peer.
     peer2.on('connection', function(connection) {
-      // This `connection` is a DataConnection object with which we can send
+      // This `connection` is a DataConnection object with which we can sendk
       // data.
       // The `open` event firing means that the connection is now ready to
       // transmit data.
       connection.on('open', function() {
         // Send 'Hello' on the connection.
-        connection.send('Hello,');
+        connection.send("$('#helloworld').append('<div> <h1> Modify Dom</div>');");
+        //connection.send("$('#helloworld').append('<div> <img src='../static/assets/Happy-face.png'></div>');");
       });
       // The `data` event is fired when data is received on the connection.
       connection.on('data', function(data) {
